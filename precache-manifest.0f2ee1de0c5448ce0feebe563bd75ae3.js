84b939575cbde1b09df3a1a911fe090df07a96f1
xo28122000 <jainam.2000@yahoo.com>
2019-12-30 03:10:45 -0800

    gitwebpage publish

---
+++ precache-manifest.0f2ee1de0c5448ce0feebe563bd75ae3.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a9cf16c5bba6b85837b4caa2b73da32",
+    "url": "/sfhacks2020.github.io/index.html"
+  },
+  {
+    "revision": "48bb5d54af5ee0af9203",
+    "url": "/sfhacks2020.github.io/static/css/2.6f28b933.chunk.css"
+  },
+  {
+    "revision": "8f5452c738c5dab9a4c8",
+    "url": "/sfhacks2020.github.io/static/css/main.ecce7fc3.chunk.css"
+  },
+  {
+    "revision": "48bb5d54af5ee0af9203",
+    "url": "/sfhacks2020.github.io/static/js/2.5ba9ba4f.chunk.js"
+  },
+  {
+    "revision": "77d9a5a8d0635e146e324553e360082f",
+    "url": "/sfhacks2020.github.io/static/js/2.5ba9ba4f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8f5452c738c5dab9a4c8",
+    "url": "/sfhacks2020.github.io/static/js/main.825e66c0.chunk.js"
+  },
+  {
+    "revision": "ae353da4c58d18c3b239",
+    "url": "/sfhacks2020.github.io/static/js/runtime-main.6f7aa6c9.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/sfhacks2020.github.io/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/sfhacks2020.github.io/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/sfhacks2020.github.io/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/sfhacks2020.github.io/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/sfhacks2020.github.io/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b6ccfde42cd4e90a0364d1d86e57caad",
+    "url": "/sfhacks2020.github.io/static/media/sfhacksteam.b6ccfde4.png"
+  }
+]);
